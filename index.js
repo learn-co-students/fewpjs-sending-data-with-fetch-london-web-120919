7264a1f16a137f64d92d3de4eeb3030e89810c4a
sarafotros <sarafotros@gmail.com>
2020-01-31 16:45:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Add your code here
+// let formData = {
+//     name,
+//     email
+// };
+
+// function submitData(formData) {
+//     return fetch("http://localhost:3000/users", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify(formData)
+//     })
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         })
+//         .catch(function (error) {
+//             alert("Bad Things!");
+//             console.log(error.message);
+//         })
+// }
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (object) {
+            document.body.innerHTML = object["id"]
+        })
+        .catch(function (error) {
+            document.body.innerHTML = error.message
+        })
+}
